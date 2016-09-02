917a22a458791a18c810366493c2f4b236759a76
syafiq <Syafiq,rezpector@gmail.com>
2016-09-02 10:57:46 +0700

    Add Hex and Binary Testing

---
+++ app/src/test/java/id/ac/ub/filkom/sekcv/appstroke/HexCalculator.java
@@ -0,0 +1,21 @@
+package id.ac.ub.filkom.sekcv.appstroke;
+
+import org.junit.Test;
+
+/**
+ * This <AppStroke> project in package <id.ac.ub.filkom.sekcv.appstroke> created by :
+ * Name         : syafiq
+ * Date / Time  : 02 September 2016, 9:18 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class HexCalculator
+{
+    @Test
+    public void testHex()
+    {
+        System.out.println(0b10011);
+        System.out.println(0b10000);
+        System.out.println(0b10011 - 0b10000 - 0b1);
+    }
+}
