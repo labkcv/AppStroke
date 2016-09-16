3a1619b4edd0c4280d1914b812c0ee01e44c313e
syafiq <Syafiq,rezpector@gmail.com>
2016-09-16 07:33:39 +0700

    Rearrange log Fix misspelled word Remove unnecessary function

---
+++ app/src/main/java/id/ac/ub/filkom/sekcv/appstroke/model/db/model/Model_User.java
@@ -0,0 +1,21 @@
+package id.ac.ub.filkom.sekcv.appstroke.model.db.model;
+
+import android.content.Context;
+
+import id.ac.ub.filkom.sekcv.appstroke.model.db.core.DatabaseModel;
+
+/**
+ * This <AppStroke> project in package <id.ac.ub.filkom.sekcv.appstroke.model.db.model> created by :
+ * Name         : syafiq
+ * Date / Time  : 30 August 2016, 12:27 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class Model_User extends DatabaseModel
+{
+
+    public Model_User(final Context context)
+    {
+        super(context);
+    }
+}
