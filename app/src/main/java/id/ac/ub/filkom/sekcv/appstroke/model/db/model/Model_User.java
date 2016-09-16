159b6a9ff9598d0e30662a8ce409bdd4d8d4cfc2
syafiq <Syafiq,rezpector@gmail.com>
2016-09-16 07:34:55 +0700

    Change class name

--- app/src/main/java/id/ac/ub/filkom/sekcv/appstroke/model/db/model/Model_User.java
+++ app/src/main/java/id/ac/ub/filkom/sekcv/appstroke/model/db/model/Model_User.java
@@ -1,6 +1,7 @@
 package id.ac.ub.filkom.sekcv.appstroke.model.db.model;
 
 import android.content.Context;
+import android.util.Log;
 
 import id.ac.ub.filkom.sekcv.appstroke.model.db.core.DatabaseModel;
 
@@ -13,9 +14,14 @@ import id.ac.ub.filkom.sekcv.appstroke.model.db.core.DatabaseModel;
  */
 public class Model_User extends DatabaseModel
 {
+    public static final String CLASSNAME = "Model_User";
+    public static final String CLASSPATH = "model.db.model";
+    public static final String TAG       = CLASSPATH + "." + CLASSNAME;
 
     public Model_User(final Context context)
     {
         super(context);
+
+        Log.d(Model_User.CLASSNAME, Model_User.TAG + ".constructor");
     }
 }
