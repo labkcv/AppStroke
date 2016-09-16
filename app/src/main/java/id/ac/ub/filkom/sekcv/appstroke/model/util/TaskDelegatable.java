15268ffdf594e0e3d37145a9cd7c35c64bb03232
syafiq <Syafiq,rezpector@gmail.com>
2016-09-16 07:29:38 +0700

    Add task delegatable interface

---
+++ app/src/main/java/id/ac/ub/filkom/sekcv/appstroke/model/util/TaskDelegatable.java
@@ -0,0 +1,13 @@
+package id.ac.ub.filkom.sekcv.appstroke.model.util;
+
+/**
+ * This <AppStroke> project in package <id.ac.ub.filkom.sekcv.appstroke.model.util> created by :
+ * Name         : syafiq
+ * Date / Time  : 15 September 2016, 2:58 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public interface TaskDelegatable
+{
+    void delegate();
+}
